11458686e9140bb866ce79cc73afe224b9567e00
lxhyl <zhangpengfan6@gmail.com>
2022-01-28 02:39:54 +0800

    v0.2:break change

---
+++ extension/src/block.js
@@ -0,0 +1,10 @@
+// 以微博id作为黑名单列表名，如果未登录，则随机生成
+function getUserWeiboId() {
+  chrome.storage.sync.get(["listId"], res => {
+    if (Object.keys(res).length > 0) return
+    let id = localStorage.getItem("WBStoreTid")
+    if (!id) id = Math.floor(Math.random() * 1000000000)
+    chrome.storage.sync.set({ listId: id })
+  })
+}
+getUserWeiboId()
---
+++ extension/src/popup/index.js
@@ -0,0 +1,37 @@
+const getDom = id => document.getElementById(id)
+
+const tip = getDom('tip')
+const listIdInput = getDom("listIdInput")
+const listIdButton = getDom('listIdButton')
+
+// 检查storage的listid
+function checkStorageListId(){
+   chrome.storage.sync.get(["listId"],res => {
+     if(Object.keys(res) === 0) return
+     tip.textContent = `当前黑名单列表ID:${res.listId}`
+     listIdInput.value = res.listId
+   })
+}
+checkStorageListId()
+
+
+listIdInput.addEventListener('input',e => {
+   listIdButton.style.cursor = e.target.value ? "grab" : "not-allowed"
+})
+// 设置列表id
+function setBlackList(){
+  const value = listIdInput.value
+  if(!value) return
+  chrome.storage.sync.set({listId:value})
+  tip.textContent = `当前黑名单ID:${value}`
+
+  listIdButton.style.backgroundColor = '#67c23a'
+  listIdButton.textContent = '成功'
+  const timer = setTimeout(() => {
+    listIdButton.style.backgroundColor = '#409eff'
+    listIdButton.textContent = '确定'
+    clearTimeout(timer)
+  },1000)
+
+}
+listIdButton.addEventListener('click',setBlackList)
