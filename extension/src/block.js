75b602c627a8d1cc9a9d2983fe51c8d196d5754e
lxhyl <zhangpengfan6@gmail.com>
2022-01-31 20:37:22 +0800

    feat:jnject button

--- extension/src/block.js
+++ extension/src/block.js
@@ -10,3 +10,35 @@ function getUserWeiboId() {
 getUserWeiboId()
 
 
+
+const isUserReg1 = /\/u\/[0-9]{10}$/g
+const isUserReg2 = /\/n\//g
+
+
+function getCommentsTag() {
+  const allATag = Array.from(document.querySelectorAll('a'))
+  const commentsTag = allATag.filter(a => {
+    // 是用户主页标签
+    let flag = isUserReg1.test(a.href) || isUserReg2.test(a.href)
+    // 跳过头像
+    flag = flag && (a.getElementsByTagName('img').length === 0)
+    if(!flag){
+      console.log(a.href)
+    }
+    return flag
+  })
+  return commentsTag
+}
+
+function injectBolckButton() {
+   const tags = getCommentsTag()
+   tags.forEach(item => {
+     if(item.WEIBO_BLOCK_FLAG) return
+     item.WEIBO_BLOCK_FLAG = true
+     const button = document.createElement('span')
+     button.textContent = '拉黑'
+     button.style.color = '#5E35B1'
+     item.appendChild(button)
+   })
+}
+setInterval(injectBolckButton, 5000)
